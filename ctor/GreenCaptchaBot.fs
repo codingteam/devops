373effdab9057f80244480e9fd31f9f3d5f1bc8c
Friedrich von Never <friedrich@fornever.me>
2023-12-20 23:36:03 +0100

    Deployment: preliminary GreenCaptchaBot schema

---
+++ ctor/GreenCaptchaBot.fs
@@ -0,0 +1,21 @@
+﻿module internal Codingteam.Ctor.GreenCaptchaBot
+
+open Fabricator.Core
+open Fabricator.Resources.Files
+
+let private hostConfigDirectory = "/opt/green-captcha-bot/"
+let private docker = docker {
+    let version = "v1.15.1"
+    fromSources <| sources {
+        gitRepository "https://github.com/ImoutoChan/GreenCaptchaBot.git"
+        reference $"tags/{version}"
+    }
+    withDockerfile "CaptchaBot/Dockerfile"
+    withTag version
+    withName "green-captcha-bot"
+    withVolume(hostPath = hostConfigDirectory, containerPath = "/app/Configuration")
+}
+
+let private configFile = FileResource(templatedFile "GreenCaptchaBot.template.json", $"{hostConfigDirectory}/appsettings.json")
+
+let resources: IResource[] = [| configFile; docker |]
--- ctor/Program.fs
+++ ctor/Program.fs
@@ -1,2 +1,18 @@
-﻿printfn "TODO: Deployment"
-exit 1
+﻿open Codingteam.Ctor
+open Fabricator.Console
+open Fabricator.Core
+
+let private cluster mode =
+    let connectionsFileName = if mode = RunMode.Verify then "connections.stub.json" else "connections.private.json"
+    [|
+        {
+            Name = "ctor"
+            Designator = Designators.fromConnectionsFile connectionsFileName "ctor"
+            Resources = GreenCaptchaBot.resources
+            Type = MachineType.Linux
+        }
+    |]
+
+let main (args: string[]): int =
+    let cluster =
+    EntryPoint.main args cluster
