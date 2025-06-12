25fe7107531654b619c925d75d7c007d33e82faa
Friedrich von Never <friedrich@fornever.me>
2025-06-13 01:33:06 +0200

    (#39) codingteam.org.ru: drop the Kubernetes setup
    We are migrating to Ansible anyway.

--- ctor/kubernetes/deploy.ps1
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2023-2025 Friedrich von Never <friedrich@fornever.me>
-#
-# SPDX-License-Identifier: MIT
-
-Set-StrictMode -Version Latest
-$ErrorActionPreference = 'Stop'
-
-kubectl apply -f $PSScriptRoot
